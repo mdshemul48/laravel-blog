94e33928188f3fd8dd0473b3709b99e9ce134650
MD. Shimul <mdshemul480@gmail.com>
2022-04-22 01:41:04 +0600

    adding welcome page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/blog', [\App\Http\Controllers\BlogPostController::class, 'index']);
+Route::get('/blog/{blogPost}', [\App\Http\Controllers\BlogPostController::class, 'show']);
 
-
-Route::get('/blog', [BlogPostController::class, 'index']);
-Route::get('/blog/{blogPost}', [BlogPostController::class, 'show']);
+Route::get('/blog/create/post', [BlogPostController::class, 'create']);
+Route::post('/blog/create/post', [BlogPostController::class, 'store']);
+Route::get('/blog/{blogPost}/edit', [BlogPostController::class, 'edit']);
+Route::put('/blog/{blogPost}/edit', [BlogPostController::class, 'update']);
+Route::delete('/blog/{blogPost}', [BlogPostController::class, 'destroy']);
