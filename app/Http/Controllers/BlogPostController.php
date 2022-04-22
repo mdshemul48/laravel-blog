0f7c397010cbbdd1cb7d6bed494f3c8b35175dbc
MD. Shimul <mdshemul480@gmail.com>
2022-04-22 11:32:20 +0600

    adding post model protected fillable form and blogPostController

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -38,7 +38,11 @@ class BlogPostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $newPost = BlogPost::create([
+            'title' => $request->title, 'body' => $request->body,
+            'user_id' => 1
+        ]);
+        return redirect('blog/' . $newPost->id);
     }
 
     /**
