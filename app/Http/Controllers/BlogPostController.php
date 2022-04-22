be03b7bbd6e43ee885465067cc111cbfd5a05308
MD. Shimul <mdshemul480@gmail.com>
2022-04-22 11:40:38 +0600

    adding edit page

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -66,7 +66,7 @@ class BlogPostController extends Controller
      */
     public function edit(BlogPost $blogPost)
     {
-        //
+        return view('blog.edit', ['post' => $blogPost]);
     }
 
     /**
